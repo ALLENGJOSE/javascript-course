0a75876d9147a753ace48164619d4ca4dd55e1ac
ALLENGJOSE <allengeorgejose@gmail.com>
2023-10-24 13:46:33 +0530

    Update strings.js

--- 2-copy-of-code/lesson-03/strings.js
+++ 2-copy-of-code/lesson-03/strings.js
@@ -1,17 +1,17 @@
 'hello'
 alert('hello');
 
-'some' + 'text'
-'some' + 'more' + 'text'
+'some' + 'text' ='sometext'
+'some' + 'more' + 'text' ='somemoretext'
 
-typeof 2
-typeof 'hello'
+typeof 2 ='number'
+typeof 'hello'='string'
 
-'hello' + 3
+'hello' + 3 ='hello3'
 '$' + 20.95 + 7.99
 '$' + (20.95 + 7.99)
 '$' + (2095 + 799) / 100
-
+//Below it is known as concatenation
 'Items (' + (1 + 1) + '): $' + (2095 + 799) / 100
 alert('Items (' + (1 + 1) + '): $' + (2095 + 799) / 100);
 
@@ -21,7 +21,9 @@ alert('Items (' + (1 + 1) + '): $' + (2095 + 799) / 100);
 'I\'m learning JavaScript'
 alert('some\ntext');
 
-`hello`
-`Items (${1 + 1}): $${(2095 + 799) / 100}`
+`hello`='hello'
+//Here ${} is known as Interpolation which allows us to enter value directly in the string
+`Items (${1 + 1}): $${(2095 + 799) / 100}`= 'Items (2): $28.94' 
+//multi line string
 `some
-text`
+text` = 'some\ntext'
